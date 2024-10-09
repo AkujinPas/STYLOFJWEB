9b1cdc6ddb596f90f0ca288aae9ed98f5203e680
GuadalupeH86 <guadalupe260500@gmail.com>
2024-10-09 04:24:05 -0600

    intento animación header

--- src/app/modules/home/header/header.component.ts
+++ src/app/modules/home/header/header.component.ts
@@ -15,17 +15,13 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  // Método para alternar la vista del carrito
   onToggleCart() {
     this.viewCart = !this.viewCart;
   }
 
-  // Método que se llamará al hacer scroll en la ventana
+  // Método que se llamará al hacer scroll
   @HostListener('window:scroll', [])
   onWindowScroll() {
-    const scrollY = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-
-    // Cambia a true si se ha hecho scroll más de 100px, ajusta este valor según sea necesario
-    this.isFixed = scrollY > 100;
+    this.isFixed = window.pageYOffset > 0; // Cambia a true si se ha hecho scroll
   }
 }
