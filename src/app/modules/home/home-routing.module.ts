5ecfdd8c8c57117eecbbc975dd91c7ca47fd95df
GuadalupeH86 <guadalupe260500@gmail.com>
2024-11-13 12:20:45 -0600

    proceso de pago

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
+import { BodyShoppingCartComponent } from './body-shopping-cart/body-shopping-cart.component';
+import { AddressProcessComponent } from './address-process/address-process.component';
 
 const routes: Routes = [
-  {path:"", component:HomeComponent}
+  { path: '', component: HomeComponent },
+  { path: 'carrito', component: BodyShoppingCartComponent },
+  { path: 'direccion-entrega', component: AddressProcessComponent }
 ];
 
 @NgModule({
