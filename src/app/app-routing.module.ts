2c1de839d507ee7386fd527e86720c6babea6a83
AkujinPas <pumas_2000_carlos@hotmail.com>
2024-11-14 06:05:37 -0600

    cambio de estructura de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddressProcessComponent } from './modules/home/address-process/address-process.component';
+import { BodyAboutUsComponent } from './modules/home/body-about-us/body-about-us.component';
+import { BodyFashionComponent } from './modules/home/body-fashion/body-fashion.component';
+import { BodyHomeComponent } from './modules/home/body-home/body-home.component';
+import { BodyShoppingCartComponent } from './modules/home/body-shopping-cart/body-shopping-cart.component';
 
 const routes: Routes = [
-  {path:"home", loadChildren:()=>import("./modules/home/home.module").then(m=>m.HomeModule)},
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' },  // Redirige la ruta raíz a 'home'
+  { path: 'inicio', component: BodyHomeComponent },
+  { path: 'carrito', component: BodyShoppingCartComponent },
+  { path: 'direccion-entrega', component: AddressProcessComponent },
+  { path: 'about-us', component: BodyAboutUsComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes)],  // Configura el enrutamiento principal con forRoot
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,69 @@
-import { NgModule } from '@angular/core';
+import { createComponent, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-
-import { HomeComponent } from './modules/home/home.component';
-import { HomeModule } from './modules/home/home.module';
 import { ProductModule } from './modules/home/product/product.module';
+
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from './modules/home/footer/footer.component';
+import { HeaderComponent } from './modules/home/header/header.component';
+import { BodyAboutUsComponent } from './modules/home/body-about-us/body-about-us.component';
+import { BodyHomeComponent } from './modules/home/body-home/body-home.component';
+import { BodyFashionComponent } from './modules/home/body-fashion/body-fashion.component';
+import { BodyContactComponent } from './modules/home/body-contact/body-contact.component';
+import { BodyShoppingCartComponent } from './modules/home/body-shopping-cart/body-shopping-cart.component';
+import { BodyOfertasComponent } from './modules/home/body-ofertas/body-ofertas.component';
+import { BodyViewProductComponent } from './modules/home/body-view-product/body-view-product.component';
+import { BodySignInComponent } from './modules/home/body-sign-in/body-sign-in.component';
+import { BodySignUpComponent } from './modules/home/body-sign-up/body-sign-up.component';
+import { InicioSesionComponent } from './modules/home/inicio-sesion/inicio-sesion.component';
+import { RegistroUsuarioComponent } from './modules/home/registro-usuario/registro-usuario.component';
+import { CarritoComponent } from './modules/home/carrito/carrito.component';
+import { PagoTarjetaComponent } from './modules/home/pago-tarjeta/pago-tarjeta.component';
+import { AnuncioinicioComponent } from './modules/home/anuncioinicio/anuncioinicio.component';
+import { PaymentProcessComponent } from './modules/home/payment-process/payment-process.component';
+import { AddressProcessComponent } from './modules/home/address-process/address-process.component';
+import { PaymentStatusComponent } from './modules/home/payment-status/payment-status.component';
+import { DashboardComponent } from './modules/home/dashboard/dashboard.component';
+import { IndexComponent } from './modules/home/product/index/index.component';
+import { CreateComponent } from './modules/home/product/create/create.component';
+import { ProductRoutingModule } from './modules/home/product/product-routing.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    
+    FooterComponent,
+    HeaderComponent,
+    BodyAboutUsComponent,
+    BodyHomeComponent,
+    BodyFashionComponent,
+    BodyContactComponent,
+    BodyShoppingCartComponent,
+    BodyOfertasComponent,
+    BodyViewProductComponent,
+    BodySignInComponent,
+    BodySignUpComponent,
+    InicioSesionComponent,
+    RegistroUsuarioComponent,
+    CarritoComponent,
+    PagoTarjetaComponent,
+    AnuncioinicioComponent,
+    PaymentProcessComponent,
+    AddressProcessComponent,
+    PaymentStatusComponent,
+    DashboardComponent,
+ 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HomeModule,
     HttpClientModule,
-    ProductModule
+    ProductModule,
+    ProductRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
